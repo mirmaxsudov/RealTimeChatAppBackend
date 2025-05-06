6a3cea4bfaf4955c90d3684cab2f0419ae5e658a
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-05-06 10:44:11 +0500

    init project

---
+++ src/main/java/uz/abdurahmon/Real/Time/Chat/App/Backend/RealTimeChatAppBackendApplication.java
@@ -0,0 +1,13 @@
+package uz.abdurahmon.Real.Time.Chat.App.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeChatAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/abdurahmon/Real/Time/Chat/App/Backend/RealTimeChatAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.abdurahmon.Real.Time.Chat.App.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
